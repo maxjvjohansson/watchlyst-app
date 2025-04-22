51f6b41cddd6f6b7ed0fed8479442740ff134bb9
TobiasAndren <tobiasandren@hotmail.com>
2025-04-22 08:24:45 +0200

    made the toggle button toggleable

--- src/components/Hero/SearchPanel.tsx
+++ src/components/Hero/SearchPanel.tsx
@@ -4,6 +4,7 @@ import InputField from "@/elements/InputField";
 
 export default function SearchPanel() {
   const [inputValue, setInputValue] = useState("");
+  const [toggleIsActive, setToggleIsActive] = useState("left");
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
@@ -12,10 +13,20 @@ export default function SearchPanel() {
   return (
     <form>
       <div className="toggle">
-        <Button type="button" className="active">
+        <Button
+          type="button"
+          onClick={() => setToggleIsActive("left")}
+          className={toggleIsActive === "left" ? "active" : ""}
+        >
           Movies
         </Button>
-        <Button type="button">Series</Button>
+        <Button
+          type="button"
+          onClick={() => setToggleIsActive("right")}
+          className={toggleIsActive === "right" ? "active" : ""}
+        >
+          Series
+        </Button>
       </div>
       <InputField
         id="search"
