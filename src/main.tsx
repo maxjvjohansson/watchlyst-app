704e35fe9f0f0d1d26d22a749135b93701c54c62
TobiasAndren <tobiasandren@hotmail.com>
2025-04-24 16:48:03 +0200

    divided global.css into multiple css files and added some movie-card styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,14 @@ import App from "./App.tsx";
 import "./variables.css";
 import "./reset.css";
 import "./global.css";
+import "./assets/css/nav-bar.css";
+import "./assets/css/main.css";
+import "./assets/css/header.css";
+import "./assets/css/form.css";
+import "./assets/css/recommended.css";
+import "./assets/css/movie-section.css";
+import "./assets/css/footer.css";
+import "./assets/css/movie-card.css";
+import "./assets/css/rating-badge.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
